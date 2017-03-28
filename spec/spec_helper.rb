e87f399e6b6adefb2b8054cccc03df6a4df85439
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-03-28 19:12:15 +0200

    Added models and tests. Added Factories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -100,28 +100,4 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-
-  # add `FactoryGirl` methods
-  config.include FactoryGirl::Syntax::Methods
-
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  # start the transaction strategy as examples are run
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
-end
-
-# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
 end
